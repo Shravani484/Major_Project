1022e5686927e1fcd34990f06a2ed7518f039ac9
Shravani484 <151320632+Shravani484@users.noreply.github.com>
2024-06-22 11:18:43 +0530

    Update map.js

--- public/javascript/map.js
+++ public/javascript/map.js
@@ -3,7 +3,7 @@ const map = new mapboxgl.Map({
         container: 'map', // container ID
         style:'mapbox://styles/mapbox/streets-v12',
         center: listing.geometry.coordinates, // starting position [lng, lat]. Note that lat must be set between -90 and 90
-        zoom: 7 // starting zoom
+        zoom: 9 // starting zoom
  });
 
 // Create a default Marker and add it to the map.
